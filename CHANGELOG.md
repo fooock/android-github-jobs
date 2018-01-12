2473b6c03523fe1b0d02ca3075eb4ae2dbcc466c
newhouse <nhitbh@gmail.com>
2018-01-12 21:45:43 +0100

    First commit

---
+++ CHANGELOG.md
@@ -0,0 +1,2 @@
+# Changelog
+* v0.1 Project started! (Fri Jan 12 21:45:43 2018)
---
+++ app/src/main/java/com/fooock/github/jobs/GithubJobsActivity.java
@@ -0,0 +1,16 @@
+package com.fooock.github.jobs;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ *
+ */
+public class GithubJobsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
