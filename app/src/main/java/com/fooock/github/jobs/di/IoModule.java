5c0b98dc205066a3719b68481d43a26a404435dc
newhouse <nhitbh@gmail.com>
2018-01-13 01:50:39 +0100

    Implemented IoModule class

--- app/src/main/java/com/fooock/github/jobs/di/IoModule.java
+++ app/src/main/java/com/fooock/github/jobs/di/IoModule.java
@@ -1,6 +1,17 @@
 package com.fooock.github.jobs.di;
 
+import com.fooock.github.jobs.GithubJobsApplication;
+import com.fooock.github.jobs.data.remote.JobsApiService;
+
+import javax.inject.Singleton;
+
 import dagger.Module;
+import dagger.Provides;
+import okhttp3.Cache;
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
  *
@@ -12,5 +23,35 @@ public class IoModule {
 
     public IoModule(String baseUrl) {
         mBaseUrl = baseUrl;
+    }
+
+    @Singleton
+    @Provides
+    Cache providesCache(GithubJobsApplication application) {
+        int size = 5 * 1024 * 1024;
+        return new Cache(application.getCacheDir(), size);
+    }
+
+    @Singleton
+    @Provides
+    OkHttpClient providesClient(Cache cache) {
+        return new OkHttpClient.Builder()
+                .cache(cache)
+                .build();
+    }
+
+    @Singleton
+    @Provides
+    Retrofit providesRetrofit(OkHttpClient client) {
+        return new Retrofit.Builder().baseUrl(mBaseUrl).client(client)
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+    }
+
+    @Singleton
+    @Provides
+    JobsApiService providesJobApiService(Retrofit retrofit) {
+        return retrofit.create(JobsApiService.class);
     }
 }
