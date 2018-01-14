38a70c67b578c260030214d5a70e0a8a3b918791
newhouse <nhitbh@gmail.com>
2018-01-14 03:25:14 +0100

    Make models from app module Parcelable

--- app/src/main/java/com/fooock/github/jobs/model/CompanyViewModel.java
+++ app/src/main/java/com/fooock/github/jobs/model/CompanyViewModel.java
@@ -1,9 +1,24 @@
 package com.fooock.github.jobs.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  *
  */
-public class CompanyViewModel {
+public class CompanyViewModel implements Parcelable {
+
+    public static final Creator<CompanyViewModel> CREATOR = new Creator<CompanyViewModel>() {
+        @Override
+        public CompanyViewModel createFromParcel(Parcel in) {
+            return new CompanyViewModel(in);
+        }
+
+        @Override
+        public CompanyViewModel[] newArray(int size) {
+            return new CompanyViewModel[size];
+        }
+    };
 
     private String mName;
     private String mUrl;
@@ -13,6 +28,12 @@ public class CompanyViewModel {
         mName = name;
         mUrl = url;
         mLogoUrl = logoUrl;
+    }
+
+    protected CompanyViewModel(Parcel in) {
+        mName = in.readString();
+        mUrl = in.readString();
+        mLogoUrl = in.readString();
     }
 
     public String getName() {
@@ -25,5 +46,17 @@ public class CompanyViewModel {
 
     public String getLogoUrl() {
         return mLogoUrl;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(mName);
+        dest.writeString(mUrl);
+        dest.writeString(mLogoUrl);
     }
 }
--- app/src/main/java/com/fooock/github/jobs/model/JobViewModel.java
+++ app/src/main/java/com/fooock/github/jobs/model/JobViewModel.java
@@ -1,11 +1,26 @@
 package com.fooock.github.jobs.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.Date;
 
 /**
  *
  */
-public class JobViewModel {
+public class JobViewModel implements Parcelable {
+
+    public static final Creator<JobViewModel> CREATOR = new Creator<JobViewModel>() {
+        @Override
+        public JobViewModel createFromParcel(Parcel in) {
+            return new JobViewModel(in);
+        }
+
+        @Override
+        public JobViewModel[] newArray(int size) {
+            return new JobViewModel[size];
+        }
+    };
 
     private String mId;
     private String mTitle;
@@ -22,6 +37,14 @@ public class JobViewModel {
         mType = type;
         mCompany = company;
         mDate = date;
+    }
+
+    protected JobViewModel(Parcel in) {
+        mId = in.readString();
+        mTitle = in.readString();
+        mLocation = in.readString();
+        mType = in.readString();
+        mCompany = in.readParcelable(CompanyViewModel.class.getClassLoader());
     }
 
     public String getId() {
@@ -46,5 +69,19 @@ public class JobViewModel {
 
     public Date getDate() {
         return mDate;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(mId);
+        dest.writeString(mTitle);
+        dest.writeString(mLocation);
+        dest.writeString(mType);
+        dest.writeParcelable(mCompany, flags);
     }
 }
