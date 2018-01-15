917e71e1770454342631c688dd0c09b1394d3eab
newhouse <nhitbh@gmail.com>
2018-01-15 22:49:39 +0100

    Added drawer layout to main view toolbar

--- app/src/main/java/com/fooock/github/jobs/activity/GithubJobsActivity.java
+++ app/src/main/java/com/fooock/github/jobs/activity/GithubJobsActivity.java
@@ -1,8 +1,15 @@
 package com.fooock.github.jobs.activity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.fooock.github.jobs.GithubJobsApplication;
 import com.fooock.github.jobs.Navigation;
@@ -13,15 +20,21 @@ import javax.inject.Inject;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import timber.log.Timber;
 
 /**
  *
  */
-public class GithubJobsActivity extends AppCompatActivity {
+public class GithubJobsActivity extends AppCompatActivity implements
+        NavigationView.OnNavigationItemSelectedListener {
 
     @BindView(R.id.toolbar) Toolbar mToolbar;
+    @BindView(R.id.drawer_layout) DrawerLayout mDrawerLayout;
+    @BindView(R.id.nav_view) NavigationView mNavigationView;
 
     @Inject Navigation mNavigation;
+
+    private ActionBarDrawerToggle mDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +44,16 @@ public class GithubJobsActivity extends AppCompatActivity {
         component().inject(this);
 
         setSupportActionBar(mToolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setHomeButtonEnabled(true);
+        }
+
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+                R.string.drawer_open, R.string.drawer_close);
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
+        mDrawerLayout.addDrawerListener(mDrawerToggle);
+        mNavigationView.setNavigationItemSelectedListener(this);
 
         if (savedInstanceState != null) return;
         mNavigation.showJobsFragment(getFragmentManager());
@@ -48,5 +71,33 @@ public class GithubJobsActivity extends AppCompatActivity {
      */
     public AppComponent component() {
         return application().component();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Timber.d("Selected: %s", item.getTitle());
+        mDrawerLayout.closeDrawers();
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            Timber.d("Selected %s", item.getTitle());
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
     }
 }
